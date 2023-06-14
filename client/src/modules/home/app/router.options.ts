0574428fa7f54eaf359e2eac2e77fe08a6c486cd
Juro Oravec <juraj.oravec.josefson@gmail.com>
2023-06-14 18:56:32 +0200

    refactor: allow to override routes

---
+++ client/src/modules/home/app/router.options.ts
@@ -0,0 +1,8 @@
+import type { RouterConfig } from '@nuxt/schema';
+
+// https://router.vuejs.org/api/interfaces/routeroptions.html
+export default <RouterConfig>{
+  routes: (_routes) => {
+    // console.log({ _routes });
+  },
+};
