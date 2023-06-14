ed1a0091d4f7c36d18ad73cf2cdc78b42c6417e3
Juro Oravec <juraj.oravec.josefson@gmail.com>
2023-06-14 13:43:31 +0200

    feat: add router

---
+++ client/src/modules/home/router.ts
@@ -0,0 +1,30 @@
+import type { PageMeta } from 'nuxt/app';
+
+import { ArrVal, enumFromArray } from '../../utils/types';
+
+const HOME_ROUTES = ['home', 'about', 'pricing'] as const;
+export type HomeRoute = ArrVal<typeof HOME_ROUTES>;
+export const HomeRouteEnum = enumFromArray(HOME_ROUTES);
+
+// Single place to define all routes + page metadata
+// Learn more https://nuxt.com/docs/guide/going-further/custom-routing
+export const homeRoutes = {
+  home: {
+    name: 'home',
+    path: '/',
+    layout: 'home-layout',
+  },
+  pricing: {
+    name: 'pricing',
+    path: '/pricing',
+    layout: 'home-layout',
+  },
+  about: {
+    name: 'about',
+    path: '/about',
+    layout: 'home-layout',
+  },
+} satisfies Record<
+  HomeRoute,
+  PageMeta & { name: HomeRoute; path: string; layout: string }
+>;
